3776d0899154ab65f589542edbe961312f2e73aa
stoicio <bhalakumaran.m@gmail.com>
2017-12-28 14:18:02 -0800

    feat:  add methods to get book data from API
    * add container vars to state to hold books belonging to different shelves
    
    * add helper method to filter books by a given shelf name
    
    * use componentDidMount hook to pull data from API and use in state.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import React from 'react'
-// import * as BooksAPI from './BooksAPI'
-import './App.css'
+import React from 'react';
+import * as BooksAPI from './BooksAPI';
+import './App.css';
+import BookShelf from './BookShelf';
 
 class BooksApp extends React.Component {
+
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+   currentlyReadingBooks: [],
+   wantToReadBooks: [],
+   readBooks: []
+  };
+
+  filterBooksByShelf = (books, shelfName) => {
+    return books.filter( book => book.shelf === shelfName);
+  };
+
+  componentDidMount() {
+
+    BooksAPI.getAll().then( books => {
+
+      let allBooks = {
+        currentlyReadingBooks: this.filterBooksByShelf(books, 'currentlyReading'),
+        wantToReadBooks: this.filterBooksByShelf(books, 'wantToRead'),
+        readBooks: this.filterBooksByShelf(books, 'read'),
+      };
+
+      this.setState(allBooks);
+    });
   }
 
   render() {
